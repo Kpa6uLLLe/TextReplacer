f6f29e00a7bb4edb347f70c0cef8372e5003f7cb
Kpa6uLLLe <103505023+Kpa6uLLLe@users.noreply.github.com>
2022-08-06 02:25:22 +0600

    Initial commit

---
+++ TextReplacer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TextReplacer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("[-help]\n[-path <text>] #Path to the directory\n[-itext <text>] #Text that should be replaced\n[-ntext <text>] #Replacement text");
+            ConsoleARGSSupport support = new ConsoleARGSSupport(args);
+            var properties = support.GetArgsValues();
+            FileTextReplacer replacer = new FileTextReplacer(properties);
+            replacer.MassReplace();
+        }
+    }
+}
