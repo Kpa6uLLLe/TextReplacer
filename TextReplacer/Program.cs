3451e6b8a93528354ede7d1e3815b7e76ea28e40
Kpa6uLLLe <103505023+Kpa6uLLLe@users.noreply.github.com>
2022-08-06 15:45:44 +0600

    Update Program.cs

--- TextReplacer/Program.cs
+++ TextReplacer/Program.cs
@@ -4,8 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            
-            Console.WriteLine("[-help]\n[-path <text>] #Path to the directory\n[-itext <text>] #Text that should be replaced\n[-ntext <text>] #Replacement text");
+            Console.WriteLine("[-help]\n[-path <text>] #Path to the directory\n[-itext <text>] #Text that should be replaced\n[-ntext <text>] #Replacement text");  
             ConsoleARGSSupport support = new ConsoleARGSSupport(args);
             var properties = support.GetArgsValues();
             FileTextReplacer replacer = new FileTextReplacer(properties);
